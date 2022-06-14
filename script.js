9361fb0a4610e99d7b3cab46f894e9bf17b62d2c
giovannyrogi <giovannirogi@gmail.com>
2022-06-14 15:12:29 +0800

    mengupload file

---
+++ script.js
@@ -0,0 +1,33 @@
+const daysEl = document.getElementById("days");
+const hoursEl = document.getElementById("hours");
+const minsEl = document.getElementById("mins");
+const secondsEl = document.getElementById("seconds");
+
+const newYears = "1 Jan 2023";
+
+function countdown() {
+    const newYearsDate = new Date(newYears);
+    const currentDate = new Date();
+
+    const distance = (newYearsDate - currentDate) / 1000;
+
+    const days = Math.floor(distance / 3600 / 24);
+    const hours = Math.floor(distance / 3600) % 24;
+    const mins = Math.floor(distance / 60) % 60;
+    const seconds = Math.floor(distance) % 60;
+
+    daysEl.innerHTML = days;
+    hoursEl.innerHTML = formatTime(hours);
+    minsEl.innerHTML = formatTime(mins);
+    secondsEl.innerHTML = formatTime(seconds);
+    
+    // console.log(days, hours, mins, seconds);
+}
+
+function formatTime(time) {
+    return time < 10 ? `0${time}`: time;
+}
+
+countdown();
+
+setInterval(countdown, 1000); 
